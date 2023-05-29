fda5248c170d37e571826040ce35dd64d4efd78c
Micah Njeru <micahnjeru1@gmail.com>
2023-05-29 13:22:29 +0300

    Initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Micah Jesse Njeru on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
